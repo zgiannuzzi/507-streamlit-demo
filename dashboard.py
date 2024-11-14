69a69ffd853ce5c3218912dc4c31f187b80c2e5a
Hants Williams <hantsawilliams@gmail.com>
2024-11-14 01:21:53 +0000

    removed time picker

--- dashboard.py
+++ dashboard.py
@@ -1,21 +1,10 @@
 import streamlit as st
 import pandas as pd
-from streamlit_date_picker import date_range_picker, PickerType, date_picker
 
 st.header("2024 AHI 507 Streamlit Example")
 st.subheader("We are going to go through a couple different examples of loading and visualization information into this dashboard")
 
 st.text("""In this streamlit dashboard, we are going to focus on some recently released school learning modalities data from the NCES, for the years of 2021.""")
-
-# Use date_range_picker to create a datetime range picker
-st.subheader('Date Range Picker')
-date_range_string = date_range_picker(picker_type=PickerType.time.string_value,
-                                      start=-30, end=0,
-                                      key='range_picker',
-                                      refresh_button={'is_show': True, 'button_name': 'Refresh last 30min',
-                                                      'refresh_date': -30,
-                                                      })
-
 
 # ## https://healthdata.gov/National/School-Learning-Modalities-2020-2021/a8v3-a3m3/about_data
 df = pd.read_csv("https://healthdata.gov/resource/a8v3-a3m3.csv?$limit=50000") ## first 1k 
