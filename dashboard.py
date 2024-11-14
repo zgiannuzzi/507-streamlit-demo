88a1dffa37bf079d34090283c9704b30e5056834
Hants Williams <hantsawilliams@gmail.com>
2024-11-14 01:21:31 +0000

    first push

---
+++ dashboard.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+from streamlit_date_picker import date_range_picker, PickerType, date_picker
+
+st.header("2024 AHI 507 Streamlit Example")
+st.subheader("We are going to go through a couple different examples of loading and visualization information into this dashboard")
+
+st.text("""In this streamlit dashboard, we are going to focus on some recently released school learning modalities data from the NCES, for the years of 2021.""")
+
+# Use date_range_picker to create a datetime range picker
+st.subheader('Date Range Picker')
+date_range_string = date_range_picker(picker_type=PickerType.time.string_value,
+                                      start=-30, end=0,
+                                      key='range_picker',
+                                      refresh_button={'is_show': True, 'button_name': 'Refresh last 30min',
+                                                      'refresh_date': -30,
+                                                      })
+
+
+# ## https://healthdata.gov/National/School-Learning-Modalities-2020-2021/a8v3-a3m3/about_data
+df = pd.read_csv("https://healthdata.gov/resource/a8v3-a3m3.csv?$limit=50000") ## first 1k 
+
+## data cleaning 
+df['week_recoded'] = pd.to_datetime(df['week'])
+df['zip_code'] = df['zip_code'].astype(str)
+
+df['week'].value_counts()
+
+## box to show how many rows and columns of data we have: 
+col1, col2, col3 = st.columns(3)
+col1.metric("Columns", df.shape[1]) 
+col2.metric("Rows", len(df))
+col3.metric("Number of unique districts/schools:", df['district_name'].nunique())
+
+## exposing first 1k of NCES 20-21 data
+st.dataframe(df)
+
+
+
+table = pd.pivot_table(df, values='student_count', index=['week'],
+                       columns=['learning_modality'], aggfunc="sum")
+
+table = table.reset_index()
+table.columns
+
+## line chart by week 
+st.bar_chart(
+    table,
+    x="week",
+    y="Hybrid",
+)
+
+st.bar_chart(
+    table,
+    x="week",
+    y="In Person",
+)
+
+st.bar_chart(
+    table,
+    x="week",
+    y="Remote",
+)
